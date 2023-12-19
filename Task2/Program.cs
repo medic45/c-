52a5c1a2f883e3b2cbd509388427a21a2ca7b702
Похламков Дмитрий Александрович <medic45rz@gmail.com>
2023-12-20 00:52:51 +0700

    lesson1:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int pruduct1 = i1 * i2;
+long pruduct2 = l1 * i1;
+double pruduct3 = d1 * i1;
+Console.WriteLine(pruduct1);
+Console.WriteLine(pruduct2);
+Console.WriteLine(pruduct3);
